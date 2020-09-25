f7573eacacd9d4d19e9df41407140de3c4b65c67
Sergio Casizzone <jambtc@gmail.com>
2020-09-25 14:46:33 +0200

    added expired to check button status

--- protected/views/tokens/view.php
+++ protected/views/tokens/view.php
@@ -47,7 +47,7 @@ $InvoiceAddress = 'https://pos.' . Utils::get_domain($http_host) . '/index.php';
 										'type'=>'raw',
 										'name'=>Yii::t('model','status'),
 										'class'=>'jam-status',
-										'value' => ( $model->status == "new" ) ?
+										'value' => ( $model->status == "new" || $model->status == 'expired') ?
 										(
 											CHtml::ajaxLink(
 											    WebApp::walletStatus($model->status),          // the link body (it will NOT be HTML-encoded.)
