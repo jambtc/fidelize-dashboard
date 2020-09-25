e1248c604b0ae3eabbd9f30ec2431bd11fe25395
Sergio Casizzone <jambtc@gmail.com>
2020-09-25 14:55:47 +0200

    fix token contr.

--- protected/controllers/TokensController.php
+++ protected/controllers/TokensController.php
@@ -1,5 +1,6 @@
 <?php
 Yii::import('libs.crypt.crypt');
+Yii::import('libs.NaPacks.Settings');
 Yii::import('libs.NaPacks.Logo');
 Yii::import('libs.NaPacks.WebApp');
 Yii::import('libs.NaPacks.SaveModels');
